535206b3e889efd7a37ce5e373bb9ae8f75ed0e9
Derek Miranda <derek.miranda@ff0000.com>
2018-07-10 17:26:22 -0700

    Added tests on:
    - basic @index resolution
    - normal requests
    - falling back on the parent directory

--- index.test.js
+++ index.test.js
@@ -1,35 +1,50 @@
-const ResolverFactory = require("enhanced-resolve").ResolverFactory;
-const plugin = require("./index.js");
-const path = require("path");
-
-describe("Simple @index resolution", () => {
-  it(
-    "basicDir/ should match to basicDir/basicDir.js",
-    resolveAndCheck("./__mocks__/@index", "./__mocks__/basicDir/basicDir.js")
-  );
-
-  it(
-    "HonorDir/ should honor package and match to HonorDir/foo.js",
-    resolveAndCheck("./__mocks__/HonorDir", "./__mocks__/HonorDir/foo.js")
-  );
-});
+const ResolverFactory = require('enhanced-resolve').ResolverFactory
+const plugin = require('./index.js')
+const path = require('path')
 
 const resolveAndCheckWithIndexName = indexName => (
-  pathToResolve,
-  expectedPath,
-  options
+	pathToResolve,
+	expectedPath,
+	options
 ) => {
-  return done => {
-    const resolver = ResolverFactory.createResolver({
-      fileSystem: require("fs"),
-      plugins: [new plugin(indexName)]
-    });
-    resolver.resolve({}, __dirname, pathToResolve, {}, (err, result) => {
-      if (err) {
-        return done(err);
-      }
-      expect(result).toEqual(path.resolve(__dirname, expectedPath));
-      done();
-    });
-  };
-};
+	return done => {
+		const resolver = ResolverFactory.createResolver({
+			fileSystem: require('fs'),
+			plugins: [new plugin(indexName)]
+		})
+		resolver.resolve({}, __dirname, pathToResolve, {}, (err, result) => {
+			if (err) {
+				return done(err)
+			}
+			expect(result).toEqual(path.resolve(__dirname, expectedPath))
+			done()
+		})
+	}
+}
+
+describe('Project with multiple indices', () => {
+	const coolResolveAndCheck = resolveAndCheckWithIndexName('cool_index')
+	it(
+		'handles normal requests normally',
+		coolResolveAndCheck(
+			'./__mocks__/cool_index/file.js',
+			'./__mocks__/cool_index/file.js'
+		)
+	)
+
+	it(
+		'@index/file.js should match to cool_index/file.js',
+		coolResolveAndCheck(
+			'./__mocks__/@index/file.js',
+			'./__mocks__/cool_index/file.js'
+		)
+	)
+
+	it(
+		'@index/file.js should match to cool_index/file.js',
+		resolveAndCheckWithIndexName('nonexistent')(
+			'./__mocks__/@index/file.js',
+			'./__mocks__/file.js'
+		)
+	)
+})
